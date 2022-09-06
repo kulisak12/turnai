ca7a8298e43eeed01eb4c97d3be250e82ec8665f
kulisak12 <kulisak12lp@gmail.com>
2022-09-06 13:58:22 +0200

    Add testing program

--- src/Main/Program.cs
+++ src/Main/Program.cs
@@ -1,9 +1,24 @@
+using TurnAi.Games.PrisonersDilemma;
+
 using System;
 
 namespace TurnAi {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int numPlayers = 3;
+            var gameFactory = Factory<PrisonersDilemmaGame>.Instance;
+            var matchMaker = new AllPairsMatchMaker(numPlayers);
+            var round = new Round(numPlayers, matchMaker, gameFactory);
+            round.OnRoundFinished = () => PrintPoints(round);
+
+            Server server = new Server(round);
+            server.Run(Config.Address);
+        }
+
+        static void PrintPoints(Round round) {
+            for (int i = 0; i < round.NumRobots; i++) {
+                Console.WriteLine($"Robot {i} has {round.GetPoints(i)} points.");
+            }
         }
     }
 }
