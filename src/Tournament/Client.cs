12ebab683f4b3710eb0e56a17fd41db006db064f
kulisak12 <kulisak12lp@gmail.com>
2022-09-06 12:58:02 +0200

    Add client API

---
+++ src/Tournament/Client.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace TurnAi {
+
+    public class Client {
+        public delegate JsonNode Strategy(JsonNode gameInfo);
+        private static readonly HttpClient client = new HttpClient();
+
+        public static void PlayRound(string robotName, Strategy strategy) {
+            var url = Config.Address + robotName;
+
+            while (true) {
+                var getResult = Get(url).Result;
+                if (getResult == null) {
+                    // no game info yet
+                    Task.Delay(500).Wait();
+                    continue;
+                }
+
+                int seq = getResult.Seq;
+                var gameInfo = getResult.GameInfo;
+                var turn = strategy(gameInfo);
+                var turnRequest = new RobotTurnRequest() {Seq = seq, Turn = turn};
+                string? postError = Post(url, turnRequest).Result;
+                if (postError == null) continue;
+                Console.WriteLine("Tournament error: " + postError);
+            }
+        }
+
+        public static async Task<RobotDataResponse?> Get(string url) {
+            try {
+                string responseBody = await client.GetStringAsync(url);
+                var response = JsonSerializer.Deserialize<RobotDataResponse>(
+                    responseBody, Config.SerializerOptions
+                );
+                if (response!.GameInfo == null) return null;
+                return response;
+            }
+            catch (HttpRequestException ex) {
+                Console.WriteLine("Get exception: " + ex.Message);
+                return null;
+            }
+        }
+
+        public static async Task<string?> Post(string url, RobotTurnRequest request) {
+            try {
+                var content = new StringContent(
+                    JsonSerializer.Serialize(request, Config.SerializerOptions)
+                );
+                var response = await client.PostAsync(url, content);
+                if (response.IsSuccessStatusCode) return null; // no error
+                string errorJson = await response.Content.ReadAsStringAsync();
+                var postResult = JsonSerializer.Deserialize<RobotErrorResponse>(
+                    errorJson, Config.SerializerOptions
+                );
+                return postResult!.Error;
+            }
+            catch (HttpRequestException ex) {
+                return "Post exception: " + ex.Message;
+            }
+        }
+    }
+}
