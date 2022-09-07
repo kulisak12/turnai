cc33353ce6bdea87a485d07133eba0eba8517929
kulisak12 <kulisak12lp@gmail.com>
2022-09-07 11:22:06 +0200

    Ensure thread safety

--- src/Tournament/Round.cs
+++ src/Tournament/Round.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 
 namespace TurnAi {
     /// <summary>
@@ -54,9 +55,14 @@ namespace TurnAi {
     public class Round : IRound {
         /// <summary>Wrapper with game metadata.</summary>
         private class Match {
+            // thread safety:
+            // Game, Seq, WasFinished: synchronized (this)
+            // PlayerIds: immutable
             public IGame Game;
-            public Dictionary<int, int> PlayerIds; // mapping between robot and player ids
             public int[] Seq;
+            public bool WasFinished = false;
+            // mapping between robot and player ids
+            public readonly Dictionary<int, int> PlayerIds;
 
             public Match(Dictionary<int, int> playerIds, IGame game) {
                 PlayerIds = playerIds;
@@ -69,21 +75,31 @@ namespace TurnAi {
             }
         }
 
+        // thread safety:
+        // matches: doesn't need to be synchronized, reads and writes are atomic
+        // totalPoints: isolated
+        // matchMaker, numRunningMatches: synchronized (matchMaker)
         private IMatchMaker matchMaker;
         private IFactory<IGame> gameFactory;
-        private Dictionary<int, Match> matches = new(); // mapping between robot ids and matches
+        private Match?[] matches; // mapping between robot ids and matches
         private int[] totalPoints;
+        private int numRunningMatches = 0;
 
         public int NumRobots { get; }
         public Action? OnRoundFinished { get; set; } = null;
         public bool IsFinished {
-            get => matches.Count == 0 && matchMaker.IsFinished;
+            get {
+                lock (matchMaker) {
+                    return numRunningMatches == 0 && matchMaker.IsFinished;
+                }
+            }
         }
 
         public Round(int numRobots, IMatchMaker matchMaker, IFactory<IGame> gameFactory) {
             NumRobots = numRobots;
             this.matchMaker = matchMaker;
             this.gameFactory = gameFactory;
+            matches = new Match?[NumRobots];
             totalPoints = new int[NumRobots];
 
             for (int i = 0; i < numRobots; i++) {
@@ -94,39 +110,51 @@ namespace TurnAi {
 
         public JsonNode? RobotGet(int robotId) {
             AssertRobotId(robotId);
+            Match? match = matches[robotId];
             // if it isn't this robot's turn, return null
-            if (!matches.ContainsKey(robotId)) return null;
-            Match match = matches[robotId];
+            if (match == null) return null;
             int playerId = match.PlayerIds[robotId];
-            if (!match.Game.MayPlay(playerId)) return null;
 
-            JsonNode gameInfo = match.Game.GetGameInfo(playerId);
-            var response = new RobotDataResponse(match.Seq[playerId], gameInfo);
+            RobotDataResponse? response = null;
+            lock (match) {
+                if (!match.Game.MayPlay(playerId)) return null;
+                JsonNode gameInfo = match.Game.GetGameInfo(playerId);
+                response = new RobotDataResponse(match.Seq[playerId], gameInfo);
+            }
+
             return JsonSerializer.SerializeToNode(response, Config.SerializerOptions);
         }
 
         public JsonNode? RobotPost(int robotId, JsonNode turnNode) {
             AssertRobotId(robotId);
-            if (!matches.ContainsKey(robotId)) {
+            Match? match = matches[robotId];
+            if (match == null) {
                 return Utility.GetErrorNode("Robot is not in a game.");
             }
-            Match match = matches[robotId];
             int playerId = match.PlayerIds[robotId];
             var request = JsonSerializer.Deserialize<RobotTurnRequest>(
                 turnNode, Config.SerializerOptions
             )!;
 
-            // prevent resubmission of the same turn
-            if (request.Seq != match.Seq[playerId]) {
-                return Utility.GetErrorNode(
-                    $"Wrong sequence number: expected {match.Seq[playerId]}, got {request.Seq}."
-                );
-            }
+            string? error = null;
+            lock (match) {
+                // prevent resubmission of the same turn
+                if (request.Seq != match.Seq[playerId]) {
+                    return Utility.GetErrorNode(
+                        $"Wrong sequence number: expected {match.Seq[playerId]}, got {request.Seq}."
+                    );
+                }
+                // prevent playing out of turn
+                if (!match.Game.MayPlay(playerId)) {
+                    return Utility.GetErrorNode("It is not your turn.");
+                }
 
-            match.Game.PlayTurn(playerId, request.Turn);
-            match.Seq[playerId]++;
-            string? error = match.Game.GetError(playerId);
-            if (match.Game.IsFinished) FinishMatch(match);
+                match.Game.PlayTurn(playerId, request.Turn);
+                match.Seq[playerId]++;
+                error = match.Game.GetError(playerId);
+                // match is finished in a task to make the response faster
+                if (match.Game.IsFinished) Task.Run(() => FinishMatch(match));
+            }
             return (error == null) ? null : Utility.GetErrorNode(error);
         }
 
@@ -136,16 +164,27 @@ namespace TurnAi {
         }
 
         private void FinishMatch(Match match) {
-            foreach (int robotId in match.PlayerIds.Keys) {
-                int playerId = match.PlayerIds[robotId];
-                totalPoints[robotId] += match.Game.GetPoints(playerId);
-                matches.Remove(robotId);
-                matchMaker.AddWaitingRobot(robotId);
+            // ensure that the match is finished only once
+            lock (match) {
+                if (match.WasFinished) return;
+                match.WasFinished = true;
             }
-            StartNewMatches();
-            if (IsFinished) OnRoundFinished?.Invoke();
+            
+            lock (matchMaker) {
+                foreach (int robotId in match.PlayerIds.Keys) {
+                    int playerId = match.PlayerIds[robotId];
+                    totalPoints[robotId] += match.Game.GetPoints(playerId);
+                    matches[robotId] = null;
+                    matchMaker.AddWaitingRobot(robotId);
+                }
+                numRunningMatches--;
+                StartNewMatches();
+                // the following will only run once, when the last match finishes
+                if (IsFinished) OnRoundFinished?.Invoke();
+            }
         }
 
+        // must be called under lock (matchMaker)
         private void StartNewMatches() {
             int[]? robotsInMatch = matchMaker.GetNextMatch();
             while (robotsInMatch != null) {
@@ -154,6 +193,7 @@ namespace TurnAi {
             }
         }
 
+        // must be called under lock (matchMaker)
         private void StartNewMatch(int[] robotsInMatch) {
             IGame game = gameFactory.Create();
             Dictionary<int, int> playerIds = new();
@@ -162,8 +202,9 @@ namespace TurnAi {
             }
             Match match = new(playerIds, game);
             foreach (int robotId in robotsInMatch) {
-                matches.Add(robotId, match);
+                matches[robotId] = match;
             }
+            numRunningMatches++;
         }
 
         // throw an exception if robotId is invalid
