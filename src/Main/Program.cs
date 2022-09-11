36844799b62904d761e3c49c686857c64598b845
kulisak12 <kulisak12lp@gmail.com>
2022-09-11 19:50:42 +0200

    Link tictactoe project

--- src/Main/Program.cs
+++ src/Main/Program.cs
@@ -1,4 +1,4 @@
-using TurnAi.Games.PrisonersDilemma;
+using TurnAi.Games.Tictactoe;
 
 using System;
 using System.Threading;
@@ -7,14 +7,15 @@ using System.Threading.Tasks;
 namespace TurnAi {
     class Program {
         static void Main(string[] args) {
-            var robotNames = new string[] { "alice", "bob", "charlie" };
+            var robotNames = new string[] { "alice", "bob"};
             int numRobots = robotNames.Length;
             Server server = new Server(DummyRound.Instance, robotNames);
 
-            var gameFactory = Factory<PrisonersDilemmaGame>.Instance;
+            var gameFactory = new TictactoeFactory(15);
             var matchMaker = new AllPairsMatchMaker(numRobots);
             var round = new Round(numRobots, matchMaker, gameFactory);
             RunRound(server, round).Wait();
+            PrintPoints(round);
         }
 
         static async Task RunRound(Server server, Round round) {
