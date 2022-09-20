fe96ff74fb44afa890380bb131b7db25d74127a7
kulisak12 <kulisak12lp@gmail.com>
2022-09-20 10:12:01 +0200

    Allow multiple rounds

--- src/Main/Program.cs
+++ src/Main/Program.cs
@@ -1,21 +1,33 @@
 using TurnAi.Games.Tictactoe;
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace TurnAi {
     class Program {
         static void Main(string[] args) {
-            var robotNames = new string[] { "alice", "bob"};
+            var robotNames = new string[] { "alice", "bob" };
             int numRobots = robotNames.Length;
+            List<int[]> points = new List<int[]>();
             Server server = new Server(DummyRound.Instance, robotNames);
-
+            int numRounds = 2;
+            TimeSpan timeBetweenRounds = TimeSpan.FromMinutes(10);
             var gameFactory = new TictactoeFactory(15);
-            var matchMaker = new AllPairsMatchMaker(numRobots);
-            var round = new Round(numRobots, matchMaker, gameFactory);
-            RunRound(server, round).Wait();
-            PrintPoints(round);
+
+            for (int i = 0; i < numRounds; i++) {
+                if (i > 0) Task.Delay(timeBetweenRounds).Wait();
+                Console.WriteLine($"Starting round {i + 1} of {numRounds}.");
+
+                var matchMaker = new AllPairsMatchMaker(numRobots);
+                var round = new Round(numRobots, matchMaker, gameFactory);
+                RunRound(server, round).Wait();
+                var roundPoints = GetRoundPoints(round);
+                points.Add(roundPoints);
+
+                Console.WriteLine($"Ending round {i + 1}.");
+            }
         }
 
         static async Task RunRound(Server server, Round round) {
@@ -27,10 +39,12 @@ namespace TurnAi {
             await serverTask;
         }
 
-        static void PrintPoints(Round round) {
+        static int[] GetRoundPoints(Round round) {
+            int[] points = new int[round.NumRobots];
             for (int i = 0; i < round.NumRobots; i++) {
-                Console.WriteLine($"Robot {i} has {round.GetPoints(i)} points.");
+                points[i] = round.GetPoints(i);
             }
+            return points;
         }
     }
 }
