0391784046dfbe336d153da668b14f1db4cc0957
kulisak12 <kulisak12lp@gmail.com>
2022-09-21 12:21:57 +0200

    Add tests for ordered match maker

--- test/TournamentTests/MatchMakerTests.cs
+++ test/TournamentTests/MatchMakerTests.cs
@@ -58,4 +58,86 @@ namespace TurnAi.TournamentTests {
             Assert.Null(matchMaker.GetNextMatch());
         }
     }
+
+    public class AllOrderedPairsMatchMakerTests {
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void TotalNumberOfMatches(int numRobots) {
+            int expectedNumMatches = numRobots * (numRobots - 1);
+            var matchMaker = new AllOrderedPairsMatchMaker(numRobots);
+            for (int i = 0; i < numRobots; i++) {
+                matchMaker.AddWaitingRobot(i);
+            }
+
+            int numMatches = 0;
+            while (!matchMaker.IsFinished) {
+                var match = matchMaker.GetNextMatch();
+                numMatches++;
+                Assert.NotNull(match);
+                foreach (int robotId in match!) {
+                    matchMaker.AddWaitingRobot(robotId);
+                }
+            }
+            Assert.Equal(expectedNumMatches, numMatches);
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void NumberOfTotalMatchesForOnePlayer(int numRobots) {
+            int expectedNumMatches = 2 * (numRobots - 1);
+            int trackedRobotId = 2;
+            var matchMaker = new AllOrderedPairsMatchMaker(numRobots);
+            for (int i = 0; i < numRobots; i++) {
+                matchMaker.AddWaitingRobot(i);
+            }
+
+            int numMatches = 0;
+            while (!matchMaker.IsFinished) {
+                var match = matchMaker.GetNextMatch();
+                foreach (int robotId in match!) {
+                    if (robotId == trackedRobotId) numMatches++;
+                    matchMaker.AddWaitingRobot(robotId);
+                }
+            }
+            Assert.Equal(expectedNumMatches, numMatches);
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void NumberOfMatchesForOnePlayerAsFirst(int numRobots) {
+            int expectedNumMatches = numRobots - 1;
+            int trackedRobotId = 2;
+            var matchMaker = new AllOrderedPairsMatchMaker(numRobots);
+            for (int i = 0; i < numRobots; i++) {
+                matchMaker.AddWaitingRobot(i);
+            }
+
+            int numMatches = 0;
+            while (!matchMaker.IsFinished) {
+                var match = matchMaker.GetNextMatch();
+                if (match![0] == trackedRobotId) numMatches++;
+                foreach (int robotId in match!) {
+                    matchMaker.AddWaitingRobot(robotId);
+                }
+            }
+            Assert.Equal(expectedNumMatches, numMatches);
+        }
+
+        [Fact]
+        public void NoAvailableMatch() {
+            var matchMaker = new AllOrderedPairsMatchMaker(2);
+            matchMaker.AddWaitingRobot(0);
+            matchMaker.AddWaitingRobot(1);
+            Assert.NotNull(matchMaker.GetNextMatch());
+            Assert.NotNull(matchMaker.GetNextMatch());
+            Assert.Null(matchMaker.GetNextMatch());
+        }
+    }
 }
